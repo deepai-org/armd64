4158c9821285a30ce3d1ad1e2bfeff8c4a64571f
kevin <kevin@deepai.org>
2026-05-25 03:55:04 +0000

    Assert foreign integer multiply

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -144,6 +144,7 @@ build_poly_elf_payloads() {
   build_poly_elf_generator
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-add.elf" 0xd2800f60 0x91002400
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-regadd.elf" 0xd2800c80 0xd28002e1 0x8b010000
+  "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-mul.elf" 0xd28000e0 0xd28000c1 0x9b017c00
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-branch.elf" 0xd2800520 0x14000002 0xd2800020 0x91000400
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-condbranch.elf" 0xd2800000 0xb5000040 0x91000400 0xd2800000 0xb4000040 0xd2800c60 0x91000c00 0xb4000040 0x91001400 0xb5000040 0xd2800c60 0x91014c00
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-loop.elf" 0xd2800060 0xd1000400 0xb5ffffe0
@@ -157,6 +158,7 @@ build_poly_elf_payloads() {
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-brk.elf" 0xd42000a0
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-add.elf" 0x01f00513 0xffc50513
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-regadd.elf" 0x06400513 0x01700593 0x00b50533
+  "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-mul.elf" 0x00700513 0x00600593 0x02b50533
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-branch.elf" 0x02900513 0x00000463 0x00100513 0x00150513
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-condbranch.elf" 0x00000513 0x00051463 0x00150513 0x00000513 0x00050463 0x06300513 0x00350513 0x00050463 0x00550513 0x00051463 0x06300513 0x05350513
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-loop.elf" 0x00300513 0xfff50513 0xfe051ee3
@@ -282,6 +284,7 @@ if [ "$RUN_POLY_APPS" = "1" ]; then
     /usr/bin/polyexec \
     /usr/lib/polyapps/aarch64-add.elf=132 \
     /usr/lib/polyapps/aarch64-regadd.elf=123 \
+    /usr/lib/polyapps/aarch64-mul.elf=42 \
     /usr/lib/polyapps/aarch64-branch.elf=42 \
     /usr/lib/polyapps/aarch64-condbranch.elf=91 \
     /usr/lib/polyapps/aarch64-loop.elf=0 \
@@ -295,6 +298,7 @@ if [ "$RUN_POLY_APPS" = "1" ]; then
     /usr/lib/polyapps/aarch64-svc.elf=0x53000701 \
     /usr/lib/polyapps/riscv-add.elf=27 \
     /usr/lib/polyapps/riscv-regadd.elf=123 \
+    /usr/lib/polyapps/riscv-mul.elf=42 \
     /usr/lib/polyapps/riscv-branch.elf=42 \
     /usr/lib/polyapps/riscv-condbranch.elf=91 \
     /usr/lib/polyapps/riscv-loop.elf=0 \
@@ -338,6 +342,7 @@ if [ "$RUN_POLY_BINFMT" = "1" ]; then
   for foreign in \
     /usr/lib/polyapps/aarch64-add.elf \
     /usr/lib/polyapps/aarch64-regadd.elf \
+    /usr/lib/polyapps/aarch64-mul.elf \
     /usr/lib/polyapps/aarch64-branch.elf \
     /usr/lib/polyapps/aarch64-condbranch.elf \
     /usr/lib/polyapps/aarch64-loop.elf \
@@ -351,6 +356,7 @@ if [ "$RUN_POLY_BINFMT" = "1" ]; then
     /usr/lib/polyapps/aarch64-svc.elf \
     /usr/lib/polyapps/riscv-add.elf \
     /usr/lib/polyapps/riscv-regadd.elf \
+    /usr/lib/polyapps/riscv-mul.elf \
     /usr/lib/polyapps/riscv-branch.elf \
     /usr/lib/polyapps/riscv-condbranch.elf \
     /usr/lib/polyapps/riscv-loop.elf \
--- tools/polybinfmt.sh
+++ tools/polybinfmt.sh
@@ -11,6 +11,7 @@ expected=""
 case "$path" in
   */aarch64-add.elf) expected=132 ;;
   */aarch64-regadd.elf) expected=123 ;;
+  */aarch64-mul.elf) expected=42 ;;
   */aarch64-branch.elf) expected=42 ;;
   */aarch64-condbranch.elf) expected=91 ;;
   */aarch64-loop.elf) expected=0 ;;
@@ -24,6 +25,7 @@ case "$path" in
   */aarch64-svc.elf) expected=0x53000701 ;;
   */riscv-add.elf) expected=27 ;;
   */riscv-regadd.elf) expected=123 ;;
+  */riscv-mul.elf) expected=42 ;;
   */riscv-branch.elf) expected=42 ;;
   */riscv-condbranch.elf) expected=91 ;;
   */riscv-loop.elf) expected=0 ;;
