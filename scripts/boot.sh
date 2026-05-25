d63efe6a7985ba56c01feedea33b61b90d334721
kevin <kevin@deepai.org>
2026-05-25 05:23:17 +0000

    Assert foreign sysinfo syscall

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -164,6 +164,7 @@ build_poly_elf_payloads() {
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-getpgid.elf" 0xd2800000 0xd2801368 0xd4000001
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-getsid.elf" 0xd2800000 0xd2801388 0xd4000001
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-getrusage.elf" 0xd2800000 0x91000041 0xd28014a8 0xd4000001 0xf9400040
+  "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-sysinfo.elf" 0x91000040 0xd2801668 0xd4000001 0xf9400040
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-mmap.elf" 0xd2800000 0xd2801bc8 0xd4000001 0xcb020000
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-mmap-store.elf" 0xd2800000 0xd2801bc8 0xd4000001 0xd28009a1 0xf9000001 0xf9400000
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-getpid.elf" 0xd2801588 0xd4000001
@@ -200,6 +201,7 @@ build_poly_elf_payloads() {
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-getpgid.elf" 0x00000513 0x09b00893 0x00000073
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-getsid.elf" 0x00000513 0x09c00893 0x00000073
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-getrusage.elf" 0x00000513 0x00060593 0x0a500893 0x00000073 0x00063503
+  "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-sysinfo.elf" 0x00060513 0x0b300893 0x00000073 0x00063503
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-mmap.elf" 0x00000513 0x0de00893 0x00000073 0x40c50533
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-mmap-store.elf" 0x00000513 0x0de00893 0x00000073 0x04d00593 0x00b53023 0x00053503
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-getpid.elf" 0x0ac00893 0x00000073
@@ -348,6 +350,7 @@ if [ "$RUN_POLY_APPS" = "1" ]; then
     /usr/lib/polyapps/aarch64-getpgid.elf=4242 \
     /usr/lib/polyapps/aarch64-getsid.elf=4242 \
     /usr/lib/polyapps/aarch64-getrusage.elf=321 \
+    /usr/lib/polyapps/aarch64-sysinfo.elf=98765 \
     /usr/lib/polyapps/aarch64-mmap.elf=0 \
     /usr/lib/polyapps/aarch64-mmap-store.elf=77 \
     /usr/lib/polyapps/aarch64-getpid.elf=4242 \
@@ -384,6 +387,7 @@ if [ "$RUN_POLY_APPS" = "1" ]; then
     /usr/lib/polyapps/riscv-getpgid.elf=4242 \
     /usr/lib/polyapps/riscv-getsid.elf=4242 \
     /usr/lib/polyapps/riscv-getrusage.elf=321 \
+    /usr/lib/polyapps/riscv-sysinfo.elf=98765 \
     /usr/lib/polyapps/riscv-mmap.elf=0 \
     /usr/lib/polyapps/riscv-mmap-store.elf=77 \
     /usr/lib/polyapps/riscv-getpid.elf=4242 \
@@ -450,6 +454,7 @@ if [ "$RUN_POLY_BINFMT" = "1" ]; then
     /usr/lib/polyapps/aarch64-getpgid.elf \
     /usr/lib/polyapps/aarch64-getsid.elf \
     /usr/lib/polyapps/aarch64-getrusage.elf \
+    /usr/lib/polyapps/aarch64-sysinfo.elf \
     /usr/lib/polyapps/aarch64-mmap.elf \
     /usr/lib/polyapps/aarch64-mmap-store.elf \
     /usr/lib/polyapps/aarch64-getpid.elf \
@@ -486,6 +491,7 @@ if [ "$RUN_POLY_BINFMT" = "1" ]; then
     /usr/lib/polyapps/riscv-getpgid.elf \
     /usr/lib/polyapps/riscv-getsid.elf \
     /usr/lib/polyapps/riscv-getrusage.elf \
+    /usr/lib/polyapps/riscv-sysinfo.elf \
     /usr/lib/polyapps/riscv-mmap.elf \
     /usr/lib/polyapps/riscv-mmap-store.elf \
     /usr/lib/polyapps/riscv-getpid.elf \
