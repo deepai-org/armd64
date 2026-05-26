cdffef57e257af4576e0c8ad24723b5b38766129
kevin <kevin@deepai.org>
2026-05-26 23:12:35 +0000

    Test foreign socket syscall payloads

--- README.md
+++ README.md
@@ -678,6 +678,7 @@ numbers currently include:
   `epoll_create1`, `epoll_ctl`, `epoll_pwait`, `openat`, `readlinkat`,
   `newfstatat`, `fstat`, `statx`, `close`, `getdents64`, `lseek`, and
   `uname`.
+- Network-style syscalls: `socket`, `sendto`, and `recvfrom`.
 - Memory/time-style syscalls: `clock_gettime`, `getrusage`, `getcpu`,
   `gettimeofday`, `sysinfo`, `prlimit64`, `getrandom`, `brk`, `munmap`,
   `mprotect`, `madvise`, and `mmap`.
--- scripts/boot.sh
+++ scripts/boot.sh
@@ -1333,6 +1333,9 @@ build_poly_elf_payloads() {
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-epoll-create1.elf" 0xd2800000 0xd2800288 0xd4000001
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-epoll-ctl.elf" 0xd2800080 0xd2800041 0xd2800062 0xd2800003 0xd28002a8 0xd4000001
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-epoll-pwait.elf" 0xd2800080 0xd2800001 0xd2800002 0xd2800003 0xd2800004 0xd2800005 0xd28002c8 0xd4000001
+  "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-socket.elf" 0xd2800040 0xd2800021 0xd2800002 0xd28018c8 0xd4000001
+  "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-sendto.elf" 0xd28000a0 0x91000021 0xd28000a2 0xd2800003 0xd2800004 0xd2800005 0xd28019c8 0xd4000001
+  "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-recvfrom.elf" 0xd28000a0 0x91000021 0xd2800082 0xd2800003 0xd2800004 0xd2800005 0xd28019e8 0xd4000001
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-fcntl.elf" 0xd2800060 0xd2800001 0xd2800002 0xd2800328 0xd4000001
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-ioctl.elf" 0xd2800020 0xd2800001 0xd2800002 0xd28003a8 0xd4000001
   "$POLY_ELF_GEN_BIN" aarch64 "$TMP_DIR/initramfs-root/usr/lib/polyapps/aarch64-openat.elf" 0xd2800000 0x91000021 0xd2800002 0xd2800708 0xd4000001
@@ -1456,6 +1459,9 @@ build_poly_elf_payloads() {
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-epoll-create1.elf" 0x00000513 0x01400893 0x00000073
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-epoll-ctl.elf" 0x00400513 0x00200593 0x00300613 0x00000693 0x01500893 0x00000073
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-epoll-pwait.elf" 0x00400513 0x00000593 0x00000613 0x00000693 0x00000713 0x00000793 0x01600893 0x00000073
+  "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-socket.elf" 0x00200513 0x00100593 0x00000613 0x0c600893 0x00000073
+  "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-sendto.elf" 0x00500513 0x00058593 0x00500613 0x00000693 0x00000713 0x00000793 0x0ce00893 0x00000073
+  "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-recvfrom.elf" 0x00500513 0x00058593 0x00400613 0x00000693 0x00000713 0x00000793 0x0cf00893 0x00000073
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-fcntl.elf" 0x00300513 0x00000593 0x00000613 0x01900893 0x00000073
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-ioctl.elf" 0x00100513 0x00000593 0x00000613 0x01d00893 0x00000073
   "$POLY_ELF_GEN_BIN" riscv "$TMP_DIR/initramfs-root/usr/lib/polyapps/riscv-openat.elf" 0x00000513 0x00058593 0x00000613 0x03800893 0x00000073
@@ -1672,6 +1678,9 @@ if [ "$RUN_POLY_EXEC" = "1" ]; then
     /usr/lib/polyapps/aarch64-epoll-create1.elf=4 \
     /usr/lib/polyapps/aarch64-epoll-ctl.elf=0 \
     /usr/lib/polyapps/aarch64-epoll-pwait.elf=0 \
+    /usr/lib/polyapps/aarch64-socket.elf=5 \
+    /usr/lib/polyapps/aarch64-sendto.elf=5 \
+    /usr/lib/polyapps/aarch64-recvfrom.elf=4 \
     /usr/lib/polyapps/aarch64-fcntl.elf=0 \
     /usr/lib/polyapps/aarch64-ioctl.elf=0 \
     /usr/lib/polyapps/aarch64-openat.elf=3 \
@@ -1768,6 +1777,9 @@ if [ "$RUN_POLY_EXEC" = "1" ]; then
     /usr/lib/polyapps/riscv-epoll-create1.elf=4 \
     /usr/lib/polyapps/riscv-epoll-ctl.elf=0 \
     /usr/lib/polyapps/riscv-epoll-pwait.elf=0 \
+    /usr/lib/polyapps/riscv-socket.elf=5 \
+    /usr/lib/polyapps/riscv-sendto.elf=5 \
+    /usr/lib/polyapps/riscv-recvfrom.elf=4 \
     /usr/lib/polyapps/riscv-fcntl.elf=0 \
     /usr/lib/polyapps/riscv-ioctl.elf=0 \
     /usr/lib/polyapps/riscv-openat.elf=3 \
@@ -2133,6 +2145,9 @@ if [ "$RUN_POLY_BINFMT" = "1" ]; then
     /usr/lib/polyapps/aarch64-epoll-create1.elf \
     /usr/lib/polyapps/aarch64-epoll-ctl.elf \
     /usr/lib/polyapps/aarch64-epoll-pwait.elf \
+    /usr/lib/polyapps/aarch64-socket.elf \
+    /usr/lib/polyapps/aarch64-sendto.elf \
+    /usr/lib/polyapps/aarch64-recvfrom.elf \
     /usr/lib/polyapps/aarch64-fcntl.elf \
     /usr/lib/polyapps/aarch64-ioctl.elf \
     /usr/lib/polyapps/aarch64-openat.elf \
@@ -2229,6 +2244,9 @@ if [ "$RUN_POLY_BINFMT" = "1" ]; then
     /usr/lib/polyapps/riscv-epoll-create1.elf \
     /usr/lib/polyapps/riscv-epoll-ctl.elf \
     /usr/lib/polyapps/riscv-epoll-pwait.elf \
+    /usr/lib/polyapps/riscv-socket.elf \
+    /usr/lib/polyapps/riscv-sendto.elf \
+    /usr/lib/polyapps/riscv-recvfrom.elf \
     /usr/lib/polyapps/riscv-fcntl.elf \
     /usr/lib/polyapps/riscv-ioctl.elf \
     /usr/lib/polyapps/riscv-openat.elf \
