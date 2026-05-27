e70a194e02536a96e65f15fccfd94a50416c6a4d
kevin <kevin@deepai.org>
2026-05-27 13:21:33 +0000

    Cover quad comparison import descriptors

--- tools/polycall_longdouble_compare_helpers_real.c
+++ tools/polycall_longdouble_compare_helpers_real.c
@@ -1,6 +1,25 @@
 static volatile double dseed = 3.5;
 static volatile float fseed = -1.25f;
 static volatile long double qseed = 4.75L;
+static volatile long double zseed = 0.0L;
+
+extern int __eqtf2(long double left, long double right);
+extern int __getf2(long double left, long double right);
+extern int __netf2(long double left, long double right);
+extern int __unordtf2(long double left, long double right);
+
+static unsigned long descriptor_compare_probe(long double x, long double y)
+{
+  long double nan = zseed / zseed;
+  unsigned long failures = 0;
+
+  failures += __eqtf2(x, x) != 0;
+  failures += __getf2(x, y) <= 0;
+  failures += __netf2(x, y) == 0;
+  failures += __unordtf2(nan, x) == 0;
+
+  return failures;
+}
 
 __attribute__((visibility("default")))
 unsigned long poly_entry(unsigned long a, unsigned long b, unsigned long c)
@@ -25,5 +44,5 @@ unsigned long poly_entry(unsigned long a, unsigned long b, unsigned long c)
     score += 17;
 
   return score + (unsigned long) (d * 10.0) +
-    (unsigned long) (f * -4.0f);
+    (unsigned long) (f * -4.0f) + descriptor_compare_probe(x, y);
 }
