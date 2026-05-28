89e3338f27d264e79b257c9c1fa612de4eeffe55
kevin <kevin@deepai.org>
2026-05-28 06:23:25 +0000

    Cover cross ISA shared stack args

--- tools/polycall_cross_needed_leaf_real.c
+++ tools/polycall_cross_needed_leaf_real.c
@@ -7,6 +7,13 @@ unsigned long poly_cross_leaf_add(unsigned long a, unsigned long b) {
 }
 
 __attribute__((visibility("default")))
+unsigned long poly_cross_leaf_add9(unsigned long a0, unsigned long a1,
+    unsigned long a2, unsigned long a3, unsigned long a4, unsigned long a5,
+    unsigned long a6, unsigned long a7, unsigned long a8) {
+  return a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + 900;
+}
+
+__attribute__((visibility("default")))
 double poly_cross_leaf_fp64(double a, double b) {
   return a + b + 7.0;
 }
--- tools/polycall_cross_needed_mid_real.c
+++ tools/polycall_cross_needed_mid_real.c
@@ -1,10 +1,14 @@
 extern unsigned long poly_cross_leaf_add(unsigned long, unsigned long);
+extern unsigned long poly_cross_leaf_add9(unsigned long, unsigned long,
+    unsigned long, unsigned long, unsigned long, unsigned long, unsigned long,
+    unsigned long, unsigned long);
 extern double poly_cross_leaf_fp64(double, double);
 extern unsigned long poly_cross_leaf_bias;
 
 __attribute__((visibility("default")))
 unsigned long poly_cross_mid_sum(unsigned long a, unsigned long b) {
-  return poly_cross_leaf_add(a, b) + 50 +
+  return poly_cross_leaf_add(a, b) +
+    poly_cross_leaf_add9(a, b, 3, 4, 5, 6, 7, 8, 9) + 50 +
     (unsigned long) poly_cross_leaf_fp64(4.5, 5.5) +
     poly_cross_leaf_bias;
 }
