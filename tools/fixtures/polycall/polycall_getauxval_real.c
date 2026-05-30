b9730f6b5562b322ad51f966bfc4cfb4c646106f
kevin <kevin@deepai.org>
2026-05-30 18:18:27 +0000

    Test cross-ISA getauxval clock ticks

--- tools/fixtures/polycall/polycall_getauxval_real.c
+++ tools/fixtures/polycall/polycall_getauxval_real.c
@@ -7,5 +7,6 @@ unsigned long poly_entry(unsigned long a0, unsigned long a1,
     unsigned long a8)
 {
   return a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 +
-    getauxval(6) + getauxval(16) + getauxval(26) + getauxval(0xdead);
+    getauxval(6) + getauxval(17) + getauxval(16) + getauxval(26) +
+    getauxval(0xdead);
 }
