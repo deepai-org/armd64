7b2a63f0e85ee84bcdc43be0d194e3316d4943e3
kevin <kevin@deepai.org>
2026-06-02 08:40:20 +0000

    Cover AArch64 pairwise CAS

--- tools/runtime/polybinfmt.sh
+++ tools/runtime/polybinfmt.sh
@@ -37,6 +37,7 @@ case "$path" in
   */aarch64-mem.elf) expected=77 ;;
   */aarch64-memwidth.elf) expected=0x100001324 ;;
   */aarch64-pair-frame.elf) expected=35 ;;
+  */aarch64-casp.elf) expected=66 ;;
   */aarch64-hints.elf) expected=27 ;;
   */aarch64-prfm.elf) expected=27 ;;
   */aarch64-cacheops.elf) expected=27 ;;
